dad2eff05d6c76343ac0ca30a27dce21d7157628
Evan Mays <me@evanmays.com>
2021-11-20 16:53:18 -0800

    clean

--- dma_host_test.py
+++ dma_host_test.py
@@ -8,7 +8,7 @@ port = "/dev/cu.usbserial-210319B26E8C1"
 SZ = 1
 BYTES_PER_FLOAT = 2
 BYTES_PER_HEADER = 1
-AMOUNT_READ = BYTES_PER_HEADER + SZ * BYTES_PER_FLOAT
+AMOUNT_READ = BYTES_PER_HEADER + SZ * SZ * BYTES_PER_FLOAT
 
 
 class PinnedDeviceMemorySpace():
@@ -19,7 +19,7 @@ class PinnedDeviceMemorySpace():
         with serial.Serial(port, timeout=4,baudrate=4800) as ser:
             # empty out any remnants of the buffers in pyserial and ftdi chip
             for _ in range(max_attempts):
-                print(_)
+                # print(_)
                 try:
                     if len(ser.read(AMOUNT_READ)) == 0:
                         success = True
@@ -36,6 +36,7 @@ class PinnedDeviceMemorySpace():
                 output_bytes = ser.read(AMOUNT_READ)
                 if len(output_bytes) == 0:
                     continue
+                assert len(output_bytes) == AMOUNT_READ
                 output_numpy = np.frombuffer(output_bytes[BYTES_PER_HEADER:], dtype='>f2')
 
                 # Check command has write flag on
@@ -45,7 +46,7 @@ class PinnedDeviceMemorySpace():
                     write_host_addr = int(output_bytes[0]) & 0x7F
                     assert write_host_addr == 120
                     # Check float value
-                    self.pinned_mem[write_host_addr:write_host_addr+1] = output_numpy
+                    self.pinned_mem[write_host_addr:write_host_addr+SZ*SZ] = output_numpy
                 else:
                     pass #TODO: support read commands here and on chip
                 print(self.pinned_mem)
