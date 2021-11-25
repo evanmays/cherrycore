8e6b209d45a3237550fbc615500bf76bb70a1cd8
Evan Mays <me@evanmays.com>
2021-11-24 17:32:11 -0800

    python supports cisa_mem_read

--- dma_host_test.py
+++ dma_host_test.py
@@ -2,18 +2,20 @@
 
 import serial
 import numpy as np
+import time
 
 port = "/dev/cu.usbserial-210319B26E8C1"
 
 SZ = 1
 BYTES_PER_FLOAT = 2
 BYTES_PER_HEADER = 1
-AMOUNT_READ = BYTES_PER_HEADER + SZ * SZ * BYTES_PER_FLOAT
+BYTES_PER_BODY = SZ * SZ * BYTES_PER_FLOAT
 
 
 class PinnedDeviceMemorySpace():
     def __init__(self):
-        self.pinned_mem = np.zeros((128,))
+        self.pinned_mem = np.zeros((128,), dtype=np.float16)
+        self.pinned_mem[120] = 15.0
         max_attempts = int(4e3)
         success = False
         with serial.Serial(port, timeout=4,baudrate=4800) as ser:
@@ -33,24 +35,30 @@ class PinnedDeviceMemorySpace():
     def start(self):
         with serial.Serial(port, timeout=4,baudrate=4800) as ser:
             while True:
-                output_bytes = ser.read(AMOUNT_READ)
-                if len(output_bytes) == 0:
-                    continue
-                assert len(output_bytes) == AMOUNT_READ, f"read {output_bytes}"
-                # print((output_bytes[BYTES_PER_HEADER:]))
-                output_numpy = np.frombuffer(output_bytes[BYTES_PER_HEADER:], dtype='>f2')
+                header_bytes = ser.read(BYTES_PER_HEADER)
+                if len(header_bytes) == 0:
+                        continue
+                assert len(header_bytes) == BYTES_PER_HEADER, f"read {header_bytes}"
+                # Check command has correct memory address
+                host_addr = int(header_bytes[0]) & 0x7F
+                assert host_addr == 120, f"received {host_addr}"
+                # Check if read or write
+                write_flag = int(header_bytes[0]) >> 7                
 
-                # Check command has write flag on
-                write_flag = int(output_bytes[0]) >> 7
                 if write_flag:
-                    # Check command has correct memory address
-                    write_host_addr = int(output_bytes[0]) & 0x7F
-                    assert write_host_addr == 120, f"received {write_host_addr}"
-                    # Check float value
-                    self.pinned_mem[write_host_addr:write_host_addr+SZ*SZ] = output_numpy
+                    body_bytes = ser.read(BYTES_PER_BODY)
+                    assert len(body_bytes) == BYTES_PER_BODY, f"read {body_bytes}"
+                    # print(body_bytes)
+                    output_numpy = np.frombuffer(body_bytes, dtype='>f2')
+                    self.pinned_mem[host_addr:host_addr+SZ*SZ] = output_numpy
+                    print(self.pinned_mem)
                 else:
-                    pass #TODO: support read commands here and on chip
-                print(self.pinned_mem)
+                    np_dat = self.pinned_mem[host_addr:host_addr+SZ*SZ]
+                    bytes_dat = np_dat.byteswap().tobytes()
+                    for i in range(len(bytes_dat)):
+                        byte = bytes_dat[i:i+1] # non sliced access isn't what you expect
+                        time.sleep(0.1) # lol
+                        ser.write(byte)
     # TODO: support tinygrad creating a new device buffer and moving data to pinned_mem (t.to_gpu())
     # TODO: support tiny grad moving data from pinned_mem to "cpu" land (t.to_cpu())
 
